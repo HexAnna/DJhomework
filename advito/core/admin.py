e1236ed2d34a32be4a437299ab32b4bf3f899ac1
AnnaBuldakova <hexusssanna@gmail.com>
2021-10-15 12:19:17 +0400

    master

--- advito/core/admin.py
+++ advito/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from core.models import Comment, Post, Profile
+
+
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Comment)
 
 # Register your models here.
--- advito/core/models.py
+++ advito/core/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+def user_avatar_path(instance, filename):
+    return f'user_{instance.user.id}/avatar/{filename}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    about = models.TextField(blank=True, max_length=700)
+    avatar = models.ImageField(upload_to=user_avatar_path)
+    
+    def __str__(self):
+        return self.user.username
 
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.first_name
+
+class Advert(models.Model):
+    title = models.CharField(max_length=70)
+    text = models.TextField(max_length=700)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title, self.author
